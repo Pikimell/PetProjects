45221d9a327fb8241e1ba4233f8bd8c8da3e88c8
Pikimell <94864615+Pikimell@users.noreply.github.com>
2022-09-27 21:08:13 +0000

    Deploying to gh-pages from @ Pikimell/PetProjects@0e0694d332ba16d20118e81732685ddece7dbc71 🚀

---
+++ string-art/StringArtGenerator.1a93cce9.js
@@ -0,0 +1,2 @@
+const NAIL_RADIUS=1,BACKGROUND_COLOR="#fff",BORDER_COLOR="#000",NAIL_COLOR="#000",PADDING=5,TOUCH_DELTA=5,CIRCLE_FORM="circle",RECT_FORM="rect",ALBUM_FORM="album",PORTRAIT_FORM="portrait",IMAGE_FORM="image",BORDER_MODE="border",GRID_MODE="grid",RANDOM_MODE="random",SCALES=[.1,.25,.5,.75,1,1.25,1.5,2,2.5,3,4,5,10,25];
+//# sourceMappingURL=StringArtGenerator.1a93cce9.js.map
